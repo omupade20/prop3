331a764e0cc7f6be20cdcc321499c25c320a7971
omupade20 <omupade16@gmail.com>
2026-01-16 03:44:21 +0530

    initial commit

---
+++ config/settings.py
@@ -0,0 +1,12 @@
+# config/settings.py
+
+"""
+Settings for connecting with the Upstox API.
+Paste your daily access token below whenever you generate it
+from the Upstox app or Developer Console.
+"""
+
+ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIzVkNVWTMiLCJqdGkiOiI2OTY3MGVjZThhMDA3NjY2NjE0Y2IwMmQiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6dHJ1ZSwiaWF0IjoxNzY4MzYxNjc4LCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3Njg0MjgwMDB9.ZapYEOn15YAj5GaXGbuShFX-xU7zF6frMw7U1yhssoM"
+
+# Base REST API URL (v2)
+API_BASE_URL = "https://api.upstox.com/v2/"
