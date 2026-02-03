90f574af22ad01b094f07ab7f8501176783e79ef
omupade20 <omupade16@gmail.com>
2026-02-03 08:41:28 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
-# config/settings.py
+# config/settin
 
 """
 Settings for connecting with the Upstox API.
@@ -6,7 +6,7 @@ Paste your daily access token below whenever you generate it
 from the Upstox app or Developer Console.
 """
 
-ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjFMyNTYifQ.eyJzdWIiOiIzVkNVWTMiLCJqdGkiOiI2OTgwMDZlYjE3NmM3YjE0OWQwMzMzYzIiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6dHJ1ZSwiaWF0IjoxNzY5OTk4MDU5LCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3NzAwNjk2MDB9.vG_4Vj8wmnX-asMZc1mWaFNdRbX2VIbzc-xuXkUUb5Y"
+ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIzVkNVWTMiLCJqdGkiOiI2OTgxNjZlMjkxNTc3YTM1ODQ2YzM5ODciLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6dHJ1ZSwiaWF0IjoxNzcwMDg4MTYyLCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3NzAxNTYwMDB9.7vXjQB18en-yosQGW6jVZmmyoHROuc_XGM7a26brz-U"
 
 # Base REST API URL (v2)
 API_BASE_URL = "https://api.upstox.com/v2/"
